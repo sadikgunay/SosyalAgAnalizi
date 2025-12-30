2dd3dddf252758231283393c64abfa4924e205d1
Sadik Gunay <sadkgny2003@gmail.com>
2025-12-30 12:46:52 +0300

    feat: Merkezilik (Degree Centrality) analizi algoritmasi eklendi

---
+++ Algorithms/Concrete/DegreeCentralityAlgorithm.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SocialNetworkGraph.App.Core;
+
+namespace SocialNetworkGraph.App.Algorithms.Concrete
+{
+    public class DegreeCentralityAlgorithm
+    {
+        public Dictionary<Node, int> Execute(Graph graph, int topN = 5)
+        {
+            var centralityScores = new Dictionary<Node, int>();
+
+            foreach (var node in graph.Nodes)
+            {
+                // Bağlantı sayısını (Degree) hesapla
+                int degree = graph.Edges.Count(e => e.Source == node || e.Target == node);
+                centralityScores[node] = degree;
+            }
+
+            // Puana göre çoktan aza sırala ve ilk N kişiyi al
+            return centralityScores.OrderByDescending(x => x.Value)
+                                   .Take(topN)
+                                   .ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
